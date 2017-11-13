0cde000c8eeab251529bedc423d27c8239f3efb7
Jaelse Ronald <ronald.smilingassassin@gmail.com>
2017-11-14 00:05:44 +0700

    asset javascript files check 5

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Web1708
     config.load_defaults 5.1
 
     config.assets.compile = true
+    config.assets.precompile =  ['*.js', '*.coffee', '*.css', '*.css.erb']
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
